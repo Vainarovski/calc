d80132b5d76f1d2b5ac64a1bb9d00af0c6487a34
Vainarovski <driwe@mail.ru>
2017-09-27 23:33:27 +0300

    new

--- src/main/java/calc/calculateClassInterface.java
+++ src/main/java/calc/calculateClassInterface.java
@@ -1,7 +1,4 @@
 package calc;
 
-/**
- * Created by Панда on 27.09.2017.
- */
 public interface calculateClassInterface {
 }
--- src/main/java/calc/main.java
+++ src/main/java/calc/main.java
@@ -3,12 +3,24 @@ package calc;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.util.Scanner;
 
 
 public class main {
     public static void main(String[] args) {
+        int numA = 0;int numB = 0;int numC = 0;
+        Scanner enterNumber = new Scanner ( System.in );
+        System.out.println ("Введите число A");
+        numA = enterNumber.nextInt();
+        System.out.println ("Введите число B");
+        numB = enterNumber.nextInt();
+        System.out.println ("Введите число C");
+        numC = enterNumber.nextInt();
         ApplicationContext context = new ClassPathXmlApplicationContext("calcConfig.xml");
         calculateClass value = (calculateClass) context.getBean("calculate");
         System.out.println(calculateClass.calcMeNow());
+
+
+
     }
 }
