41ca5f6f8b4bd8738998bec131c55ada0c118c10
Vainarovski <driwe@mail.ru>
2017-09-29 12:14:25 +0300

    new

--- src/main/java/calc/main.java
+++ src/main/java/calc/main.java
@@ -5,22 +5,27 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.util.Scanner;
 
-
 public class main {
     public static void main(String[] args) {
-        int numA = 0;int numB = 0;int numC = 0;
+        ApplicationContext context = new ClassPathXmlApplicationContext ( "calcConfig.xml" );
+        calculateClass value = (calculateClass) context.getBean ( "calculate" );
+        System.out.println ( calculateClass.calcMeNow () );
+
+    }
+
+
+    private int numA = 0;
+    private int numB = 0;
+    private int numC = 0;
+
+    public void enterABC() {
         Scanner enterNumber = new Scanner ( System.in );
-        System.out.println ("Введите число A");
-        numA = enterNumber.nextInt();
-        System.out.println ("Введите число B");
-        numB = enterNumber.nextInt();
-        System.out.println ("Введите число C");
-        numC = enterNumber.nextInt();
-        ApplicationContext context = new ClassPathXmlApplicationContext("calcConfig.xml");
-        calculateClass value = (calculateClass) context.getBean("calculate");
-        System.out.println(calculateClass.calcMeNow());
-
-
+        System.out.println ( "Введите число A" );
+        numA = enterNumber.nextInt ();
+        System.out.println ( "Введите число B" );
+        numB = enterNumber.nextInt ();
+        System.out.println ( "Введите число C" );
+        numC = enterNumber.nextInt ();
 
     }
 }
